2a43c38e60ea4ae35a72a88aa6bbc8eb30a71e23
Lad1mir <ladimir.matveev@gmail.com>
2021-08-30 19:43:40 +0300

    First exerсise

---
+++ Lesson1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int getNumber = Convert.ToInt32(Console.ReadLine());
+                bool isPrimeNumber = IsPrimeNumber(getNumber);
+                if (isPrimeNumber)
+                {
+                    Console.WriteLine("Число простое");
+                }
+                else
+                {
+                    Console.WriteLine("Число не простое");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Строка не является числом");
+            }
+            Console.ReadLine();
+        }
+
+        static public bool IsPrimeNumber(int number)
+        {
+            int d = 0;
+            int i = 2;
+            if (number <= 1)
+            {
+                return false;
+            }
+            
+            while (true)
+            {
+                if (i < number)
+                {
+                    if (number % i == 0)
+                    {
+                        d++;
+                        i++;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+                else
+                {
+                    return (d == 0);
+                }
+            }
+        }
+    }
+}
